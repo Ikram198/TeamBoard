7dc2f795a6499092aec2e5e5783a2b788ec6742c
Ikram Malik <126634784+Ikram198@users.noreply.github.com>
2026-06-25 11:55:39 +0530

    Add Password and Verification Token fields to user model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -14,6 +14,14 @@ const userschema = new Schema({
         type : String,
         required : true
     },
+    Password: {
+        type : String,
+        required : true
+    },
+    Verification_Token: {
+        type : String,
+        required : true
+    },
     email: {
         type: String,
         required: true,
