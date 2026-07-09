57328379c938afff8ed03f70c725b4ea6dc23000
Ikram Malik <126634784+Ikram198@users.noreply.github.com>
2026-07-09 09:57:15 +0530

    Enhance auth routes with verification and password features
    Added routes for user verification and password management.

--- src/routs/auth.routes.js
+++ src/routs/auth.routes.js
@@ -1,10 +1,16 @@
 import express from 'express';
 const router = express.Router();
 
-import {register_user, login_user} from "../controllers/auth.controllers.js";
+import {register_user, login_user, Forget_Password, Reset_password , User_register_verification } from "../controllers/auth.controllers.js";
 
 router.post('/register' , register_user);
 
+router.post('/register_verification' , User_register_verification);
+
 router.post('/login', login_user);
+
+router.post('/forget_password' , Forget_Password);
+
+router.post('/reset_password' , Reset_password);
 
 export default router;
