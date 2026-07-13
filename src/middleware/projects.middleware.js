bb5989787e451eaffd42283d9b961f00d4d5c87d
Ikram Malik <126634784+Ikram198@users.noreply.github.com>
2026-07-13 10:19:33 +0530

    Update project middleware for JWT verification
    Refactor middleware to use jwt.verify for token validation and improve access control checks.

--- src/middleware/projects.middleware.js
+++ src/middleware/projects.middleware.js
@@ -1,17 +1,41 @@
 import { jwtDecode } from "jwt-decode";
 
+const projects_middleware = asyncHandler(async(req , res, next) => {
+    const {access_token} = req.cookies.access_token;
+    if (!access_token){
+        throw new ApiError(401, "user is not authorised for project access ")
+    }
+    const decodedPayload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decodedPayload;
+    
+    next();
+    
+})
+
 
 const create_project_middleware = asyncHandler(async(req, res, next)=>{
-    const {access_token} = req.cookies;
+    const {access_token} = req.cookies.access_token;
     if (!access_token){
         throw new ApiError(401, "user is not authorised for creating new project")
     }
-    const decodedPayload = jwtDecode(access_token);
+    const decodedPayload = jwt.verify(token, process.env.JWT_SECRET);
     // console.log(decodedPayload);
-    const projects = decodedPayload.projects || [];
-    return ApiResponse(201, "user is authorised for creating new project and has access to projects: " + projects)
-
     next();
 })
 
-export {create_project_middleware}
+const access_project_middleware = asyncHandler(async(req, res, next) => {
+    const {access_token} = req.cookies.access_token;
+    if (!access_token){
+        throw new ApiError(401, "user is not authorised for creating new project")
+    }
+    const decodedPayload = jwt.verify(token, process.env.JWT_SECRET);
+    const project = req.params.token;
+    const Is_member = decodedPayload.projects.find(project);
+    if(!Is_member){
+        throw new ApiError(401, "user is not a part of this project")
+    }
+    next();
+    
+})
+
+export { projects_middleware , create_project_middleware , access_project_middleware}
