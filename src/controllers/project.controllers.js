ed4d3603c71d15aaba4db2b479917c0eb1376a2a
Ikram Malik <126634784+Ikram198@users.noreply.github.com>
2026-07-14 10:10:10 +0530

    Refactor project controller methods for error handling

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -4,10 +4,10 @@ import { ApiResponce } from "../utils/API-response.js";
 import {Project} from "../models/project.model.js";
 import {User }from "../models/user.models.js";
 
+
 const all_project = asyncHandler(async(req,res) => {
-    const projects = Project.findall(user);
+    const {projects} = req.user
     ApiResponce(201, "user is a part of " +projects)
-    
 })
 
 
@@ -16,11 +16,11 @@ const create_project = asyncHandler(async(req, res)=>{
     const createdby = req.user.name;
     const createdat = Date.now();
     if (!project_title || !project_description){
-        return ApiError(301, "please enter project_title and project_description")
+        throw new ApiError(301, "please enter project_title and project_description")
     }
     const project = await Project.insertone({name:project_title, description:project_description, createdby:createdby, createdat:createdat})
     if(!project){
-        return ApiError(501, "error in creating new project in database")
+        throw new ApiError(501, "error in creating new project in database")
     }
     ApiResponce(200, "successfully created new project")
 })
@@ -30,7 +30,12 @@ const create_project = asyncHandler(async(req, res)=>{
 const project = asyncHandler(async(req, res) => {
     const project = req.params.project;
     const the_project = Project.findone(project);
+    if (!the_project) {
+        throw new ApiError(501, "error in fetching project from database project may be deleted by someone")
+    }
+    
     ApiResponce(201, "displaying the project"+the_project)
+    
     
 })
 
