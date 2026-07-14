f213d4c97d395ecc2c732eec4d63caf2425ed186
Ikram Malik <126634784+Ikram198@users.noreply.github.com>
2026-07-14 09:59:44 +0530

    Update projects.middleware.js

--- src/middleware/projects.middleware.js
+++ src/middleware/projects.middleware.js
@@ -33,13 +33,12 @@ const access_project_middleware = asyncHandler(async(req, res, next) => {
         throw new ApiError(401, "user is not authorised for creating new project")
     }
     const decodedPayload = jwt.verify(token, process.env.JWT_SECRET);
-    const project = req.params.token;
+    const project = req.params.project;
     const Is_member = decodedPayload.projects.find(project);
     if(!Is_member){
         throw new ApiError(401, "user is not a part of this project")
     }
     next();
-    
 })
 
 export { projects_middleware , create_project_middleware , access_project_middleware}
