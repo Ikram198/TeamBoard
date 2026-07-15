f5ea96cc850f92da7d80888db735b7ca6e530402
Ikram Malik <126634784+Ikram198@users.noreply.github.com>
2026-07-15 10:53:58 +0530

    Change project source to request parameters
    Updated project retrieval to use params instead of user data.

--- src/controllers/note.controllers.js
+++ src/controllers/note.controllers.js
@@ -4,7 +4,7 @@ import { asyncHandler } from "../utils/Async-Handler.js";
 const create_note = asyncHandler(async (req, res) => { 
     const { Note_content , attachments } = req.body;
     const createdby = req.user.name;
-    const project = req.user.project;
+    const project = req.params.project;
     if(!Note_content){
         return ApiError(401, "please enter a valid note content")
     }
