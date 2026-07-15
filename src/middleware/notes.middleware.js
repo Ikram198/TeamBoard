4e3a1323bb9581281dd6914d7fdbcc5e230e5979
Ikram Malik <126634784+Ikram198@users.noreply.github.com>
2026-07-15 10:53:14 +0530

    Update notes.middleware.js

--- src/middleware/notes.middleware.js
+++ src/middleware/notes.middleware.js
@@ -1,11 +1,18 @@
 import ApiError from "../utils/API-error.js";
-import ApiResponse from "../utils/API-response.js";
 import { asyncHandler } from "../utils/Async-Handler.js";
-const notes_middleware = asyncHandler(async(req, res)=>{
-  const {access_token} = req.cookies;
-  if (!access_token){
-    throw new ApiError(401, "user is not authorised for notes section")
-  
-  
-  }
+
+const notes_middleware = asyncHandler(async(req , res, next) => {
+    const {access_token} = req.cookies.access_token;
+    const project = req.params.project;
+    if (!access_token){
+        throw new ApiError(401, "user is not authorised for project access ")
+    }
+   const decodedPayload = jwt.verify(access_token, process.env.JWT_SECRET);
+    const project = req.params.project;
+    const Is_member = decodedPayload.projects.find(project);
+    if(!Is_member){
+        throw new ApiError(401, "user is not a part of this project")
+    }
+    next();
+    
 })
