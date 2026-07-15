bc9bb5dbdda8352c8a807ecd0087ad645e035c0f
Ikram Malik <126634784+Ikram198@users.noreply.github.com>
2026-07-15 10:32:52 +0530

    Add task middleware for project authorization
    Implement task middleware to authorize project access based on JWT.

---
+++ src/middleware/task.middleware.js
@@ -0,0 +1,19 @@
+import {Project} from "../models/project.model.js";
+
+const task_middleware = asyncHandler(async(req , res, next) => {
+    const {access_token} = req.cookies.access_token;
+    const project = req.params.project;
+    if (!access_token){
+        throw new ApiError(401, "user is not authorised for project access ")
+    }
+    const decodedPayload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decodedPayload;
+    await const project = Project.findone(project);
+    if(! decodedPayload.name == project.created_by){
+      throw new ApiError(401, "user is not authorised for creating task ")
+    }
+  
+    
+    next();
+    
+})
