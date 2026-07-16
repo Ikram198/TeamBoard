f37ff6df65ea3d69e9b79d252668bb77c287ebde
Ikram Malik <126634784+Ikram198@users.noreply.github.com>
2026-07-16 09:37:11 +0530

    Add middleware to project routes

--- src/routs/project.routes.js
+++ src/routs/project.routes.js
@@ -1,12 +1,14 @@
-import create_project from '../controllers/project.controllers.js'
+import {create_project , all_project , project} from '../controllers/project.controllers.js';
+import { projects_middleware , create_project_middleware , access_project_middleware} from ../middleware/projects.middleware.js';
+
 import express from 'express';
 
 const router = express.Router()
 
-router.get('/projects', All_projects)
+router.get('/projects',projects_middleware , all_project)
 
-router.get('/create_project', create_project)
+router.get('/create_project', create_project_middleware ,  create_project)
 
-router.get('/:project', project)
+router.get('/:project', access_project_middleware ,  project)
 
 export default router
