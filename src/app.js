96876cc25a2e1d307f11a992558acd7b0c84cf41
Ikram Malik <126634784+Ikram198@users.noreply.github.com>
2026-07-16 09:45:05 +0530

    Import and use additional routers in app.js
    Added new routes for notes, projects, and tasks.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
-import auth_router from "./routs/auth.routes.js"
 import health_router from "./routs/healthcheck.routes.js"
+import auth_router from "./routs/auth.routes.js"
+import note_router from "./routs/note.routes.js"
+import project_router from "./routs/project.routes.js"
+import task_router from "./routs/task.routes.js"
 const app = express();
 app.use(express.json());
 
@@ -10,9 +13,11 @@ app.get('/', (req, res) => {
 });
 
 
-app.use('/api/v1/authentication', auth_router);
 app.use('/api/v1/health', health_router);
 app.use('/api/v1/authentication', auth_router);
+app.use('/api/v1/authentication', note_router);
+app.use('/api/v1/authentication', project_router);
+app.use('/api/v1/authentication', task_router);
 
 
 
