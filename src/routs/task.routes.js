5fed2aa66bc26c20dd289af455551e97c9294ad9
Ikram Malik <126634784+Ikram198@users.noreply.github.com>
2026-07-16 09:38:42 +0530

    Update task.routes.js

--- src/routs/task.routes.js
+++ src/routs/task.routes.js
@@ -1,8 +1,10 @@
 import {create_task} from "../controllers/task.controller.js"
+import {task_middleware} from "../middleware/task.middleware.js"
+
 import express from "express"
 
 const router = express.Router();
 
-router.get('/task', create_task)
+router.get('/task', task_middleware , create_task)
 
 export default router 
