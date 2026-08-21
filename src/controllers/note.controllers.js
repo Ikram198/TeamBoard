4d39d51e1b7256b0b2816639a740dd4bcd5fe23b
Ikram Malik <126634784+Ikram198@users.noreply.github.com>
2026-08-21 11:11:47 +0530

    Refactor note controller to include additional functions

--- src/controllers/note.controllers.js
+++ src/controllers/note.controllers.js
@@ -2,13 +2,11 @@ import {ApiError} from "../utils/API-error.js";
 import ApiResponse from "../utils/API-response.js";
 import {Note }from "../models/note.models.js";
 import { asyncHandler } from "../utils/Async-Handler.js";
+
 const create_note = asyncHandler(async (req, res) => { 
     const { Note_content , attachments } = req.body;
     const createdby = req.user.name;
-
-    // const created_by = req.user.name;
     const created_by = "6a759441c7090ec87e66b262";
-
     const project = req.params.project;
     if(!Note_content){
         return ApiError(401, "please enter a valid note content")
@@ -20,4 +18,17 @@ const create_note = asyncHandler(async (req, res) => {
     return res.status(201).json(new ApiResponse(201, "successfully created the project note"))
 })
 
-export { create_note };
+
+const get_note_by_id = asyncHandler(async (req, res) => {
+    
+})
+
+const get_all_notes = asyncHandler(async (req, res) => {
+    
+})
+
+const delete_note = asyncHandler(async (req, res) => {
+    
+})
+
+export { create_note , get_note_by_id , get_all_notes , delete_note };
