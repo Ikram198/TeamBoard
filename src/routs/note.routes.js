ae8a453aee458fbdb13362b02d437bd937ca1049
Ikram Malik <126634784+Ikram198@users.noreply.github.com>
2026-08-21 11:02:10 +0530

    Update note.routes.js

--- src/routs/note.routes.js
+++ src/routs/note.routes.js
@@ -1,11 +1,19 @@
-import { create_note } from '../controllers/note.controllers.js'
-import {notes_middleware} from '../middleware/notes.middleware.js'
+import { create_note , get_note_by_id , get_all_notes , delete_note } from '../controllers/note.controllers.js'
+import {notes_middleware , notes_access_middleware} from '../middleware/notes.middleware.js'
 
 import express from 'express';
 
 const router = express.Router()
 
-router.post('/note/:project',notes_middleware, create_note)
+router.post('/create_note/:project',notes_middleware, create_note)
+
+router.post('/get_note/:project',notes_middleware, get_note_by_id)
+
+router.post('/all_project_notes/:project',notes_middleware, get_project_notes)
+
+router.post('/all_notes',notes_middleware, get_all_notes)
+
+router.post('/delete_note/:project',notes_middleware, delete_note)
 
 
 export default router
